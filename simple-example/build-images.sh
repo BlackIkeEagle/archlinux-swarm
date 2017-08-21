901719b1df45988d044821ba70cb4a9f5b1040f0
BlackEagle <ike.devolder@gmail.com>
2017-08-21 20:50:00 +0200

    simple-sample :: build images without cache to demonstrate stack update
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- simple-example/build-images.sh
+++ simple-example/build-images.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-docker build -f Dockerfile-nginx -t blackikeeagle/swarm-sample-nginx .
-docker build -f Dockerfile-php -t blackikeeagle/swarm-sample-php .
+docker build --no-cache -f Dockerfile-nginx -t blackikeeagle/swarm-sample-nginx .
+docker build --no-cache -f Dockerfile-php -t blackikeeagle/swarm-sample-php .
 
 docker push blackikeeagle/swarm-sample-nginx
 docker push blackikeeagle/swarm-sample-php
