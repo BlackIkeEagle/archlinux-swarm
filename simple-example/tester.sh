fb7dc975f68ff5d6df973ba718559a7d82232213
BlackEagle <ike.devolder@gmail.com>
2017-08-21 20:39:15 +0200

    simple-example :: tester dont crash when curl fails and wait max 2 seconds
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- simple-example/tester.sh
+++ simple-example/tester.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env sh
 
-set -e
-
 while true; do
-    curl http://192.168.254.41; echo
-    curl http://192.168.254.42; echo
-    curl http://192.168.254.43; echo
+    curl -m2 http://192.168.254.41; echo
+    curl -m2 http://192.168.254.42; echo
+    curl -m2 http://192.168.254.43; echo
 done
 
